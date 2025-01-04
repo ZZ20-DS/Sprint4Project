b3a859d1d561b6131707ced7372d7c6ef5425ca0
ZZ20-DS <zebazafer@yahoo.com>
2025-01-04 11:18:27 -0500

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
+
 
 
 # Title of the app centered
@@ -29,36 +29,36 @@ def main():
     st.write("Top 10 Models by Frequency:")
     st.table(model_counts.head(10))
 
-    # Create a histogram for the top 10 models
-    st.write("Histogram: Top 10 Most Frequent Car Models")
-    fig, ax = plt.subplots()
-    model_counts.head(10).plot(kind='bar', ax=ax, color='skyblue')
-    ax.set_title("Most Frequent Car Models")
-    ax.set_xlabel("Car Model")
-    ax.set_ylabel("Frequency")
-    st.pyplot(fig)
-     # Data for top 5 popular car colors
-    paint_colors = {
-        'white': 10029,
-        'black': 7692,
-        'silver': 6244,
-        'grey': 5037,
-        'blue': 4475
-    }
+    # # Create a histogram for the top 10 models
+    # st.write("Histogram: Top 10 Most Frequent Car Models")
+    # fig, ax = plt.subplots()
+    # model_counts.head(10).plot(kind='bar', ax=ax, color='skyblue')
+    # ax.set_title("Most Frequent Car Models")
+    # ax.set_xlabel("Car Model")
+    # ax.set_ylabel("Frequency")
+    # st.pyplot(fig)
+    #  # Data for top 5 popular car colors
+    # paint_colors = {
+    #     'white': 10029,
+    #     'black': 7692,
+    #     'silver': 6244,
+    #     'grey': 5037,
+    #     'blue': 4475
+    # }
 
     colors = list(paint_colors.keys())
     frequencies = list(paint_colors.values())
 
-    # Create a scatter plot for top 5 popular car colors
-    st.write("Scatter Plot: Top 5 Popular Car Colors")
-    fig, ax = plt.subplots()
-    ax.scatter(colors, frequencies, color='green', s=100)
-    ax.set_title("Top 5 Popular Car Colors")
-    ax.set_xlabel("Car Colors")
-    ax.set_ylabel("Frequency")
-    for i, txt in enumerate(frequencies):
-        ax.annotate(txt, (colors[i], frequencies[i]), textcoords="offset points", xytext=(0,10), ha='center')
-    st.pyplot(fig)
+    # # Create a scatter plot for top 5 popular car colors
+    # st.write("Scatter Plot: Top 5 Popular Car Colors")
+    # fig, ax = plt.subplots()
+    # ax.scatter(colors, frequencies, color='green', s=100)
+    # ax.set_title("Top 5 Popular Car Colors")
+    # ax.set_xlabel("Car Colors")
+    # ax.set_ylabel("Frequency")
+    # for i, txt in enumerate(frequencies):
+    #     ax.annotate(txt, (colors[i], frequencies[i]), textcoords="offset points", xytext=(0,10), ha='center')
+    # st.pyplot(fig)
 
 # Run the app
 if __name__ == "__main__":
