acd87d66829161aa2aa69dd04088e5498f18709b
ZZ20-DS <zebazafer@yahoo.com>
2025-01-04 11:57:12 -0500

    new

--- app.py
+++ app.py
@@ -22,3 +22,41 @@ fig_histogram = px.histogram(df, x='price', title='Distribution of Vehicle Price
 
 st.plotly_chart(fig_histogram)
 
+ 
+
+show_expensive_cars = st.checkbox('Show only cars priced above $20,000')
+
+ 
+
+if show_expensive_cars:
+
+    filtered_df = df[df['price'] > 20000]
+
+    fig = px.histogram(filtered_df, x='type', title='Stock of Car Type', labels={'type':'Car Type'})
+
+    fig.update_layout(yaxis_title="Amount in Stock")
+
+    st.plotly_chart(fig)
+
+else:
+
+    fig = px.histogram(df, x='type', title='Stock of Car Type', labels={'type':'Car Type'})
+
+    fig.update_layout(yaxis_title="Amount in Stock")
+
+    st.plotly_chart(fig)
+
+st.write(filtered_df)
+
+
+
+
+
+
+
+
+
+
+
+
+ 
