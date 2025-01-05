ae860aad4389c466f451f538e11624205b4e9a4a
ZZ20-DS <zebazafer@yahoo.com>
2025-01-04 23:02:44 -0500

    new

--- app.py
+++ app.py
@@ -12,57 +12,39 @@ df = pd.read_csv('vehicles_us.csv')
 # Title of the app centered
 st.title('DRIVING FORCES: What affects Car Sales and Pricing')
 
+st.write("The following shown here are a collection of graphs and charts featuring the data shown in the vehicle dataset here.")
+
  
 
-# Show data in the app
-st.write(df)
+fig_histogram = px.histogram(df, x='price', title='Distribution of Vehicle Prices')
 
-# histogram of the types of vehicles by manufacturer
-st.subheader('Histogram of the types of vehicles by manufacturer')
-fig = px.histogram(df, x='manufacturer', color='type')
-# plot the histogram
-st.plotly_chart(fig)
+st.plotly_chart(fig_histogram)
 
-# histogram of price distribution between manufacturers
-st.subheader('Histogram of price distribution between manufacturers')
-# drop down menu for selecting the manufacturer 1 and 2 
-# index 1 and 2 are used to set default values for the drop down menu
-manufacturer1 = st.selectbox('Manufacturer 1', df['manufacturer'].unique(), index=1)
-manufacturer2 = st.selectbox('Manufacturer 2', df['manufacturer'].unique(), index=2)
-# create a normalized histogram checkbox
-normalized = st.checkbox('Normalized')
-# create a histogram with manufacturer1 and manufacturer2 input
-fig = px.histogram()
-fig.add_trace(go.Histogram(x=df[df['manufacturer'] == manufacturer1]['price'], name=manufacturer1, opacity=0.75, histnorm='percent'))
-fig.add_trace(go.Histogram(x=df[df['manufacturer'] == manufacturer2]['price'], name=manufacturer2, opacity=0.75, histnorm='percent'))
-# normalize the histogram if the checkbox is checked
-if normalized:
-    fig.update_layout(barmode='overlay')
-    fig.update_traces(opacity=0.75)
-# x-axis title
-fig.update_xaxes(title_text='Price')
-# y-axis title
-fig.update_yaxes(title_text='Percentage')
-# plot the histogram
-st.plotly_chart(fig)
+ 
 
+show_expensive_cars = st.checkbox('Show only cars priced above $20,000')
 
+ 
 
-# scatter plot matrix 
-st.subheader('Scatter plot matrix')
-# drop down for each dimension 
-# index 1, 2, and 3 are used to set default values for the drop down menu
-x_axis = st.selectbox('X axis', df.columns, index=1)
-y_axis = st.selectbox('Y axis', df.columns, index=2)
-# drop down for the color
-color = st.selectbox('Color', df.columns, index=3)
-# subheader for the scatter plot matrix that automatically updates
-st.subheader(f'Scatter plot matrix of {x_axis} and {y_axis} by {color}')
-# create the scatter plot matrix
-fig = px.scatter_matrix(df, dimensions=[x_axis, y_axis], color=color)
-# plot the scatter plot matrix
-st.plotly_chart(fig)
-   
+if show_expensive_cars:
 
+    filtered_df = df[df['price'] > 20000]
 
+    fig = px.histogram(filtered_df, x='type', title='Stock of Car Type', labels={'type':'Car Type'})
+
+    fig.update_layout(yaxis_title="Amount in Stock")
+
+    st.plotly_chart(fig)
+
+else:
+
+    fig = px.histogram(df, x='type', title='Stock of Car Type', labels={'type':'Car Type'})
+
+    fig.update_layout(yaxis_title="Amount in Stock")
+
+    st.plotly_chart(fig)
+
+st.write(filtered_df)
+
+ 
  
