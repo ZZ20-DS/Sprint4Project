072d76e9828f34138b70890bae74d365a32db826
ZZ20-DS <zebazafer@yahoo.com>
2025-01-05 10:39:38 -0500

    new

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv('vehicles_us.csv')
+df = pd.read_csv('vehicles_us.csv', engine='c')
 
-df = df[df['price'].apply(lambda x: str(x).isdigit())] 
 df['price'] = pd.to_numeric(df['price'], errors='coerce')  
 df = df.dropna(subset=['price'])  
 df['price'] = df['price'].astype(int)  
 
 st.header("Vehicle Data Dashboard")
 
-st.write("The following shown here are a collection of graphs and charts featuring the data shown in the vehicle dataset here.")
+st.write("The following are a collection of graphs and charts featuring the data shown in the vehicle dataset.")
 
 fig_histogram = px.histogram(df, x='price', title='Distribution of Vehicle Prices')
 st.plotly_chart(fig_histogram)
@@ -24,6 +23,7 @@ if show_expensive_cars:
     fig.update_layout(yaxis_title="Amount in Stock")
     st.plotly_chart(fig)
 
+    # Display filtered dataframe
     st.write("Filtered Dataframe (Cars priced above $20,000):")
     st.write(filtered_df)
 else:
@@ -31,9 +31,17 @@ else:
     fig.update_layout(yaxis_title="Amount in Stock")
     st.plotly_chart(fig)
 
+    # Display unfiltered dataframe
     st.write("Unfiltered Dataframe:")
     st.write(df)
- 
+
+
+
+
+
+
+
+
  
 
 
