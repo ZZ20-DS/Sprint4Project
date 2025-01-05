80c23ee6e6320c1875a8d79e0e519cef8b86109f
ZZ20-DS <zebazafer@yahoo.com>
2025-01-04 19:31:17 -0500

    new

--- app.py
+++ app.py
@@ -1,55 +1,30 @@
 import streamlit as st
-
 import pandas as pd
-
 import plotly.express as px
-
- 
 
 df = pd.read_csv('vehicles_us.csv')
 
- 
-
 st.header("Vehicle Data Dashboard")
-
- 
 
 st.write("The following shown here are a collection of graphs and charts featuring the data shown in the vehicle dataset here.")
 
- 
-
 fig_histogram = px.histogram(df, x='price', title='Distribution of Vehicle Prices')
-
 st.plotly_chart(fig_histogram)
-
- 
 
 show_expensive_cars = st.checkbox('Show only cars priced above $20,000')
 
- 
-
 if show_expensive_cars:
-
     filtered_df = df[df['price'] > 20000]
-
-    fig = px.histogram(filtered_df, x='type', title='Stock of Car Type', labels={'type':'Car Type'})
-
+    fig = px.histogram(filtered_df, x='type', title='Stock of Car Type', labels={'type': 'Car Type'})
     fig.update_layout(yaxis_title="Amount in Stock")
-
     st.plotly_chart(fig)
-
+    st.write(filtered_df)  # Show filtered data when checkbox is checked
 else:
-
-    fig = px.histogram(df, x='type', title='Stock of Car Type', labels={'type':'Car Type'})
-
+    fig = px.histogram(df, x='type', title='Stock of Car Type', labels={'type': 'Car Type'})
     fig.update_layout(yaxis_title="Amount in Stock")
-
     st.plotly_chart(fig)
 
-st.write(filtered_df)
-
-
-
+ 
 
 
 
